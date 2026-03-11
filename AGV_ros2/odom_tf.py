e719514f25703a4d49106c75897ae2a4a5259aa1
EdwinZM <edwinzwa@hotmail.com>
2026-03-11 10:57:01 +0100

    added working odom transform publisher for sim

--- AGV_ros2/odom_tf.py
+++ AGV_ros2/odom_tf.py
@@ -25,8 +25,14 @@ class OdomTF(Node):
         t.header.frame_id = 'odom'
         t.child_frame_id = 'base_footprint'
         
-        t.transform.translation = msg.pose.pose.position
-        t.transform.rotation = msg.pose.pose.orientation
+        t.transform.translation.x = msg.pose.pose.position.x
+        t.transform.translation.y = msg.pose.pose.position.y
+        t.transform.translation.z = msg.pose.pose.position.z
+
+        t.transform.rotation.x = msg.pose.pose.orientation.x
+        t.transform.rotation.y = msg.pose.pose.orientation.y
+        t.transform.rotation.z = msg.pose.pose.orientation.z
+        t.transform.rotation.w = msg.pose.pose.orientation.w
         
         self.br.sendTransform(t)
         
